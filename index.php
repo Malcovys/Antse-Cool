9b29a199bece710b0f5050fa05b2ba50605bc7d1
Malcovys <malcovys22.aps@gmail.com>
2023-06-12 10:45:56 +0300

    log in router

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once('src/controllers/pages/homepage.php');
 require_once('src/controllers/pages/singuppage.php');
+require_once('src/controllers/pages/singinpage.php');
 require_once('src/controllers/models/saveStudent.php');
 
 
@@ -11,19 +12,12 @@ try {
     if (isset($_GET['action']) && $_GET['action'] !== '') {
         
         if ($_GET['action'] === 'singup') {
-           
             if (isset($_POST['first_name']) && $_POST['first_name'] !== '') {
-
                 if (isset($_POST['last_name']) && $_POST['last_name'] !== '') {
-
                     if (isset($_POST['id']) && $_POST['id'] !== '') {
-
                         if (isset($_POST['email']) && $_POST['email'] !== '') {
-
                             if (isset($_POST['group']) && $_POST['group'] !== '') {
-
                                 if (isset($_POST['promotion']) && $_POST['promotion'] !== '') {
-
                                     if (isset($_POST['password']) && $_POST['password'] !== '') {
                                         
                                         saveStudent($_POST);
@@ -31,30 +25,28 @@ try {
                                     } else {
                                         throw new Exception('Password is required.');
                                     }
-
                                 } else {
                                     throw new Exception('Promotion is empty.');
                                 }
-
                             } else {
                                 throw new Exception('Group is required.');
                             }
-
                         } else {
                             throw new Exception('E-mail is required.');
                         }
-
                     } else {
                         throw new Exception('Your matricul is empty.');
                     }
-
                 } else {
                     throw new Exception('What is your last name ?');
                 }
-
             } else {
                 throw new Exception('Your first name please');
             }
+        }
+
+        elseif ($_GET['action'] === 'login') {
+            singinPage();
         }
 
     } else {
